a0e0593a943a363a069f970cb8c72c3910dde54f
izno94 <iznosc2@gmail.com>
2021-03-10 18:50:06 +0100

    integrated js

---
+++ IMCjs.js
@@ -0,0 +1,10 @@
+function calIMC() {
+
+    let  x = document.getElementById('x').value,
+         y = document.getElementById('y').value,
+         result;
+         result = x / y**2
+
+         alert(result);
+}
+
