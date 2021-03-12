8e6e5c824add0da12bd2453fe322cac67ae61a27
izno94 <iznosc2@gmail.com>
2021-03-12 10:56:47 +0100

    minor change

--- IMCjs.js
+++ IMCjs.js
@@ -3,8 +3,13 @@ function calIMC() {
     let  x = document.getElementById('x').value,
          y = document.getElementById('y').value,
          result;
-         result = x / y**2
-
+         result = x / y**2 *10000
+          
          alert(result);
+
+         if (result = "")
+         {
+            alert(" tu dois entrer un valeur ")
+         }
 }
 
