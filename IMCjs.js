fad03248f3f2e88d1e55aed308965161a6bb80b9
unknown <iznosc2@gmail.com>
2021-04-07 18:15:02 +0200

    test

--- IMCjs.js
+++ IMCjs.js
@@ -1,15 +1,28 @@
 function calIMC() {
 
-    let  x = document.getElementById('x').value,
-         y = document.getElementById('y').value,
-         result;
-         result = x / y**2 *10000
-          
-         alert(result);
+   let x = document.getElementById('x').value,
+      y = document.getElementById('y').value,
+      result = Math.round( x / y ** 2 * 10000 ) 
 
-         if (result = "")
-         {
-            alert(" tu dois entrer un valeur ")
-         }
+console.log(result)
+
+   if (result <= 18) {
+      alert(result + " Trop maigre !!!!! MANGE MANGE !!!!! ")
+   }
+   if (result > 18  && result <= 25) {
+      alert(result + " Tu es au summum")
+   }
+   if (result > 25 && result <= 30) {
+      alert(result + " tu es en surpoids")
+   }
+   if (result > 30 && result <= 35) {
+      alert(result + " Obésité modérée")
+   }
+   if (result > 35 && result <= 40) {
+      alert(result + " Obésité sévère")
+   }
+   if (result > 40)
+   {
+      alert(result + " Obésité morbide ou massive")
+   }
 }
-
