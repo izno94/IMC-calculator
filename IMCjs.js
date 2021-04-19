b961dc5791e44fe13ae152e106e2d56855f8f09c
izno94 <iznosc2@gmail.com>
2021-04-19 11:49:44 +0200

    add innerHTML and removes Alert

--- IMCjs.js
+++ IMCjs.js
@@ -2,27 +2,33 @@ function calIMC() {
 
    let x = document.getElementById('x').value,
       y = document.getElementById('y').value,
-      result = Math.round( x / y ** 2 * 10000 ) 
+      result = Math.round(x / y ** 2 * 10000)
 
-console.log(result)
+   console.log(result)
 
    if (result <= 18) {
-      alert(result + " Trop maigre !!!!! MANGE MANGE !!!!! ")
+
+      document.getElementById("show").innerHTML = result + " Trop maigre !!!!! MANGE MANGE !!!!! "
    }
-   if (result > 18  && result <= 25) {
-      alert(result + " Tu es au summum")
+   if (result > 18 && result <= 25) {
+
+      document.getElementById("show").innerHTML = result + " Tu es au summum "
+
    }
    if (result > 25 && result <= 30) {
-      alert(result + " tu es en surpoids")
+
+      document.getElementById("show").innerHTML = result + " tu es en surpoid "
    }
    if (result > 30 && result <= 35) {
-      alert(result + " Obésité modérée")
+
+      document.getElementById("show").innerHTML = result + " Obésité modérée "
    }
    if (result > 35 && result <= 40) {
-      alert(result + " Obésité sévère")
+
+      document.getElementById("show").innerHTML = result + " Obésité sévère "
    }
-   if (result > 40)
-   {
-      alert(result + " Obésité morbide ou massive")
+   if (result > 40) {
+
+      document.getElementById("show").innerHTML = result + " Obésité morbide ou massive "
    }
 }
