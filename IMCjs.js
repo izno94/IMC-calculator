b939f9ceba60b1e2d96e6924d56e4433412a5894
izno94 <iznosc2@gmail.com>
2021-04-20 15:43:19 +0200

    add conditional styles

--- IMCjs.js
+++ IMCjs.js
@@ -8,27 +8,32 @@ function calIMC() {
 
    if (result <= 18) {
 
-      document.getElementById("show").innerHTML = result + " Trop maigre !!!!! MANGE MANGE !!!!! "
+      document.getElementById("show").innerHTML = result + " <br/> Trop maigre !!!!! MANGE MANGE !!!!! "
+      document.getElementById("show").className = "underWeight"
    }
    if (result > 18 && result <= 25) {
 
-      document.getElementById("show").innerHTML = result + " Tu es au summum "
-
+      document.getElementById("show").innerHTML = result + " <br/> Tu es au summum "
+      document.getElementById("show").className = "optimal"
    }
    if (result > 25 && result <= 30) {
 
-      document.getElementById("show").innerHTML = result + " tu es en surpoid "
+      document.getElementById("show").innerHTML = result + " <br/> tu es en surpoid "
+      document.getElementById("show").className = "overweight"
    }
    if (result > 30 && result <= 35) {
 
-      document.getElementById("show").innerHTML = result + " Obésité modérée "
+      document.getElementById("show").innerHTML = result + " <br/> Obésité modérée "
+      document.getElementById("show").className = "modérée"
    }
    if (result > 35 && result <= 40) {
 
-      document.getElementById("show").innerHTML = result + " Obésité sévère "
+      document.getElementById("show").innerHTML = result + " <br/> Obésité sévère "
+      document.getElementById("show").className = "danger"
    }
    if (result > 40) {
 
-      document.getElementById("show").innerHTML = result + " Obésité morbide ou massive "
+      document.getElementById("show").innerHTML = result + " <br/> Obésité morbide ou massive "
+      document.getElementById("show").className = "morbide"
    }
 }
